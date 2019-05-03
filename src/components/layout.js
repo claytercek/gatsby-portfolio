6eb45aadd48b62ceb9d69cba653bda7add71755a
Clay Tercek <clay.tercek@gmail.com>
2019-05-02 22:18:17 -0400

    added static query to layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,23 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
-export default ({ children }) => (
-  <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
-    <h3>MySweetSite</h3>
-    {children}
-  </div>
-)
+export default ({ children }) => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  )
+
+  return (
+    <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+      <h3>{data.site.siteMetadata.title}</h3>
+      {children}
+    </div>
+  )
+}
