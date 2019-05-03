fab971c9b2af22c07e93de6787d61826af3eba1d
Clay Tercek <clay.tercek@gmail.com>
2019-05-02 22:21:14 -0400

    basic linking

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 
 export default ({ data }) => {
@@ -7,15 +7,13 @@ export default ({ data }) => {
   return (
     <Layout>
       <div>
-        <h1>Amazing Pandas Eating Things</h1>
-        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
-          <div key={node.id}>
+          <Link to={node.fields.slug} key={node.id}>
             <h3>
               {node.frontmatter.title} <span> — {node.frontmatter.date}</span>
             </h3>
             <p>{node.excerpt}</p>
-          </div>
+          </Link>
         ))}
       </div>
     </Layout>
@@ -29,6 +27,9 @@ export const query = graphql`
       edges {
         node {
           id
+          fields {
+            slug
+          }
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
