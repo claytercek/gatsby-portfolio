9ea2bb1a0d1ff7ba7a0af9c5dc33857f6afef426
Clay Tercek <code@claytercek.com>
2019-06-29 16:16:22 -0400

    general accessibility improvements

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -23,7 +23,7 @@ class Header extends Component {
           <Link className={styles.logo} to={"/"}>
             {this.props.data.site.siteMetadata.title}
           </Link>
-          <button onClick={this.handleClick} />
+          <button onClick={this.handleClick} aria-label="menu toggle"/>
         </div>
         <nav>
           <ul>
