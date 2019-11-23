0b3f28adc33b3ce645596ead1ff9bce0c6202a95
Clay Tercek <claytercek@Clays-MacBook-Pro.local>
2019-11-23 13:22:35 -0500

    fix build issues

--- src/pages/journal-list.js
+++ src/pages/journal-list.js
@@ -2,45 +2,58 @@ import React from "react"
 import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Headline from "../components/headline"
+import {css} from "@emotion/core"
 
+const listStyle = theme => (css`
+  list-style-type: none;
+  padding: 0;
+  padding-left: ${theme.pad}px;
+`)
 
 export default ({ data }) => {
-    return (
-      <Layout>
-        <SEO />
-        <main className={"mainContent"}>
-          <ul>
-            {data}
-            {/* {data.allMarkdownRemark.edges.map(({ node }) => (
+  return (
+    <Layout>
+      <SEO />
+      <main className={"mainContent"}>
+        <ul css={listStyle}>
+          {data.allMarkdownRemark.edges.map(({ node }, index) => {
+            let i = (1 + index).toString().padStart(2, '0');
+            return (
               <li key={node.id} >
-                <Link >
-                  <Img  fluid={node.frontmatter.image.childImageSharp.fluid} />
-                  <h3 >{node.frontmatter.title}</h3>
-                  <h4 >{node.frontmatter.subtitle}</h4>
+                <Link to={node.fields.slug}>
+                  <Headline
+                    title={node.frontmatter.title}
+                    subtitle={i + " " + node.frontmatter.type}
+                  />
                 </Link>
               </li>
-            ))} */}
-          </ul>
-        </main>
-      </Layout>
-    )
-  }
-  
-  export const query = graphql`
+            )
+          })}
+        </ul>
+      </main>
+    </Layout>
+  )
+}
+
+export const query = graphql`
     query {
-      allMarkdownRemark {
+        allMarkdownRemark(filter: {
+            fields: {type: {eq: "journal"}}
+        }) {
         totalCount
         edges {
-          node {
+            node {
             id
             fields {
-              slug
+                slug
             }
             frontmatter {
-              title
+                title
+                type
             }
           }
         }
       }
-    }
+    }  
   `
